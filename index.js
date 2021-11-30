af9bc6fb3db58a3d884a9faa66ea35874d2bb329
depth <depth@localhost.localdomain>
2021-12-01 04:16:52 +0530

    Using axios to get rpi website index file as response and link the local css file to it

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
-
+const axios = require('axios')
 
 const PORT = 5000
+const url = "https://rugpullindex.com"
 
 const app = express()
+
+axios(url)
+  .then(response => {
+
+    const html = response.data
+    const new_html = html.replace("/93218339c23.css", `/local.css`)
+    //the above line will change the href file location in the html file
+    //to the local css file in the design system repo
+
+})
 
 app.use(express.static(__dirname)) 
 
