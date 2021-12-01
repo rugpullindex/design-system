aced85a3b4e8cc1f657b7bafee403ac43955288e
depth <depth@localhost.localdomain>
2021-12-01 05:42:55 +0530

    Using fs write the cache directory

--- index.js
+++ index.js
@@ -16,9 +16,14 @@ axios(url)
     //the above line will change the href file location in the html file
     //to the local css file in the design system repo
     
+    //Make the cache directory
+    fs.mkdir(path.join(__dirname, 'cache'), function(err) {
+      if(err) { return console.log(err) }
+    })
+
     //Save the new html file to cache folder in design system repo
     fs.writeFile('cache/index.html', new_html, function(err) { 
-      if(err) { return console.log(err); }
+      if(err) { return console.log(err) }
     })
     
   })
